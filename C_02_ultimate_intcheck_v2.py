a2ebf845772930cfbdbd373e9e7cb8e89dd14d3e
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2024-03-27 09:53:35 +1300

    Add files via upload

---
+++ C_02_ultimate_intcheck_v2.py
@@ -0,0 +1,42 @@
+def int_check(question, low=None, high=None, exit_code=None):
+
+    # if any integer is allowed...
+    if low is None and high is None:
+        error = "Please enter an integer "
+
+    # if the number needs to be more than an
+    # integer (ie: rounds / 'high number')
+    elif low is not None and high is None:
+        error = (f"Please enter an integer that is "
+                 f"more then / equal to {low}")
+
+    # if the number needs to between low and high
+    else:
+        error = (f"Please enter an integer that is "
+                 f" is between {low} and {high} (inclusive)")
+
+    while True:
+        response = input(question).lower()
+
+        # check for infinite mode / exit code
+        if response == exit_code:
+            return response
+
+        try:
+            response = int(response)
+
+            # Check the integer is not too low...
+            if low is not None and response < low:
+                print(error)
+
+            # Check response is more than the low number
+            elif high is not None and response > high:
+                print(error)
+
+            # if response is valid, return it
+            return response
+
+        except ValueError:
+            print(error)
+
+# Main Routine goes here
